81dc03beafbeccb211628c5f54cf2eecd9d5c769
PEDRO HENRIQUE DE FREITAS BARBOSA <pedro_hf_barbosa@estudante.sesisenai.org.br>
2024-08-19 17:07:48 -0300

    adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='titulo1'>hello react JS fofinho</h1>
+    <p className='Paragrafo'> Para com isso por favor </p>
+
+    
+    
+      
+    </>
+  )
+}
+
+export default App
